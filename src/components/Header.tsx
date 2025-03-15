e5fbe769c2a0f6d5982c3f5bad798b15f45ca929
4samad <smd2k15@gmail.com>
2025-03-16 01:33:17 +0530

    :bento: add favicons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
 
@@ -6,13 +7,12 @@ export default function Header() {
     <div className="navbar fixed top-0 right-0 left-0 z-10 m-auto max-w-6xl">
       <div className="navbar-start">
         <Link href="/" className="p-4">
-          {/*<Image
-            src="/assets/images/logo.png"
-            alt="Logo"
-            width={100}
-            height={100}
-            className="brightness-0 invert"
-          />*/}
+          <Image
+            src="/assets/aimdao-logo.svg"
+            alt="AIMDAO Logo"
+            width={48}
+            height={48}
+          />
         </Link>
       </div>
       <div className="navbar-end p-4">
