c49bcfeaca8393b37a9f2f73738490623c82d2b7
4samad <smd2k15@gmail.com>
2025-03-16 18:05:00 +0530

    :bento: update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,14 @@ export default function Header() {
   return (
     <div className="navbar fixed top-0 right-0 left-0 z-10 m-auto max-w-6xl">
       <div className="navbar-start">
-        <Link href="/" className="p-4">
+        <Link
+          href="/"
+          className="bg-opacity-50 rounded-full bg-white px-4 py-2 backdrop-blur-3xl"
+        >
           <Image
-            src="/assets/aimdao-logo.svg"
+            src="/assets/aimdao-logo-full.svg"
             alt="AIMDAO Logo"
-            width={48}
+            width={120}
             height={48}
           />
         </Link>
