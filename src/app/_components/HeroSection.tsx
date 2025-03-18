591751f38ac6a4b24f3e9c77e7596047717be84d
4samad <smd2k15@gmail.com>
2025-03-18 16:46:48 +0530

    :lipstick: update styles

--- src/app/_components/HeroSection.tsx
+++ src/app/_components/HeroSection.tsx
@@ -1,27 +1,10 @@
 export default function HeroSection() {
   return (
-    <div className="hero pt-32 pb-20 lg:pt-40">
-      <div className="hero-content flex-wrap gap-12 lg:flex-row-reverse lg:gap-8">
-        <div className="relative z-10 max-w-xl">
-          {/* Glow effect container */}
-          <div className="absolute inset-0 -z-10 scale-105 animate-pulse rounded-full bg-gradient-to-r from-purple-500 via-red-500 to-blue-500 opacity-70 blur-xl"></div>
+    <div className="hero py-24">
+      <div className="hero-content relative flex-wrap gap-12 lg:gap-8">
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
 
-          {/* Video container */}
-          <div className="relative z-10 overflow-hidden rounded-full shadow-md">
-            <video
-              preload="auto"
-              loop
-              muted
-              autoPlay
-              playsInline
-              className="h-full w-full max-w-5xl object-cover"
-            >
-              <source src="/assets/hero-video.mp4" type="video/mp4" />
-            </video>
-          </div>
-        </div>
-
-        <div className="max-w-xl">
+        <div className="mt-12 max-w-xl">
           <h1 className="text-5xl font-bold">
             Fastcharge ⚡ with
             <br />
@@ -34,6 +17,25 @@ export default function HeroSection() {
             <a href="/whitepaper" className="btn mt-8 bg-neutral-200">
               Whitepaper
             </a>
+          </div>
+        </div>
+
+        <div className="relative z-10 max-w-xl">
+          {/* Glow effect container */}
+          <div className="absolute inset-0 -z-10 scale-105 animate-pulse rounded-full bg-gradient-to-r from-purple-500 via-red-500 to-blue-500 opacity-70 blur-xl"></div>
+          {/* Video container */}
+          <div className="relative z-10 overflow-hidden shadow-md">
+            <div className="bg-base-300 absolute top-0 right-0 left-0 h-px w-[2000px]" />
+            <video
+              preload="auto"
+              loop
+              muted
+              autoPlay
+              playsInline
+              className="flip aspect-square w-full max-w-5xl -scale-x-100 object-cover"
+            >
+              <source src="/assets/hero-video.mp4" type="video/mp4" />
+            </video>
           </div>
         </div>
       </div>
